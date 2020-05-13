c1f1b5a901335d6e9602dfb618f2e1119c7263a3
jerry's mac <jerrychangcn@outlook.com>
2020-05-13 17:03:30 +0800

    add the AMP page.

---
+++ application/controllers/Amp.php
@@ -0,0 +1,72 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * Class Amp
+ */
+class Amp extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Amp_family_model');
+	}
+
+	/**
+	 *
+	 */
+	public function index()
+	{
+		if ($this->input->method(TRUE) == 'GET' or $this->input->method(TRUE) == 'OPTIONS') {
+			$pageSize = $this->input->get('pageSize', TRUE);
+			$currentPage = $this->input->get('currentPage', TRUE);
+			$id = $this->input->get('id', TRUE);
+			$sequence = $this->input->get('sequence', TRUE);
+			$minLength = $this->input->get('minLength', TRUE);
+			$maxLength = $this->input->get('maxLength', TRUE);
+			$minpI = $this->input->get('minpI', TRUE);
+			$maxpI = $this->input->get('maxpI', TRUE);
+			$minCharge = $this->input->get('minCharge', TRUE);
+			$maxCharge = $this->input->get('maxCharge', TRUE);
+
+			$data['count'] = $this->Amp_family_model->count($id, $sequence, $minLength, $maxLength, $minpI, $maxpI, $minCharge, $maxCharge);
+			$data['AMP'] = $this->Amp_family_model->read($id, $pageSize, $currentPage, $sequence, $minLength, $maxLength, $minpI, $maxpI, $minCharge, $maxCharge);
+
+			foreach ($data['AMP'] as $key => $value) {
+				$data['AMP'][$key]['pI'] = trim(rtrim(sprintf("%.4f", $value['pI']), '0'), '.');
+				$data['AMP'][$key]['charge'] = trim(rtrim(sprintf("%.4f", $value['charge']), '0'), '.');
+			}
+
+			$this->output
+				->set_status_header(200)
+				->set_content_type('application/json')
+				->set_output(json_encode($data));
+		} else {
+			$this->output
+				->set_status_header(405)
+				->set_content_type('application/json');
+		}
+	}
+
+//	/**
+//	 * @return bool
+//	 */
+//	public function test()
+//	{
+//		if ($this->input->method(TRUE) == 'POST' or $this->input->method(TRUE) == 'OPTIONS') {
+//			$command = escapeshellcmd('ls');
+//			$data = array();
+//			$data['command'] = $command;
+//			exec($command, $data['output'], $data['return_val']);
+//			$this->output
+//				->set_status_header(200)
+//				->set_content_type('application/json')
+//				->set_output(json_encode($data));
+//		} else {
+//			$this->output
+//				->set_status_header(405)
+//				->set_content_type('application/json')
+//				->set_output();
+//		}
+//	}
+}
---
+++ application/models/Amp_family_model.php
@@ -0,0 +1,105 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * Class Amp_family_model
+ */
+class Amp_family_model extends CI_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
+
+	/**
+	 * @param $id
+	 * @param $pageSize
+	 * @param $currentPage
+	 * @param $sequence
+	 * @param $minLength
+	 * @param $maxLength
+	 * @param $minpI
+	 * @param $maxpI
+	 * @param $minCharge
+	 * @param $maxCharge
+	 * @return mixed
+	 */
+	public function read($id, $pageSize, $currentPage, $sequence, $minLength, $maxLength, $minpI, $maxpI, $minCharge, $maxCharge)
+	{
+		$this->db->select('amp_family.*,amp_feature.pI,amp_feature.charge');
+		$this->db->from('amp_family');
+		$this->db->join('amp_feature', 'amp_family.AMP_ID=amp_feature.AMP_ID', 'left');
+		if ($id != '') {
+			$this->db->like('amp_family.AMP_ID', $id);
+			$this->db->or_like('amp_family.Family_id', $id);
+		}
+		if ($sequence != '') {
+			$this->db->like('amp_family.Sequence', $sequence);
+		}
+		if ($minLength != 1) {
+			$this->db->where('amp_family.Length >=', $minLength);
+		}
+		if ($maxLength != 100) {
+			$this->db->where('amp_family.Length <=', $maxLength);
+		}
+		if ($minpI != 0) {
+			$this->db->where('amp_feature.pI >=', $minpI);
+		}
+		if ($maxpI != 15) {
+			$this->db->where('amp_feature.pI <=', $maxpI);
+		}
+		if ($minCharge != -50) {
+			$this->db->where('amp_feature.Charge >=', $minCharge);
+		}
+		if ($maxCharge != 50) {
+			$this->db->where('amp_feature.Charge <=', $maxCharge);
+		}
+		$this->db->limit($pageSize, ($currentPage - 1) * $pageSize);
+		return $this->db->get()->result_array();
+	}
+
+	/**
+	 * @param $id
+	 * @param $sequence
+	 * @param $minLength
+	 * @param $maxLength
+	 * @param $minpI
+	 * @param $maxpI
+	 * @param $minCharge
+	 * @param $maxCharge
+	 * @return mixed
+	 */
+	public function count($id, $sequence, $minLength, $maxLength, $minpI, $maxpI, $minCharge, $maxCharge)
+	{
+		$this->db->select('amp_family.*,amp_feature.pI,amp_feature.charge');
+		$this->db->from('amp_family');
+		$this->db->join('amp_feature', 'amp_family.AMP_ID=amp_feature.AMP_ID', 'left');
+		if ($id != '') {
+			$this->db->like('amp_family.AMP_ID', $id);
+			$this->db->or_like('amp_family.Family_id', $id);
+		}
+		if ($sequence != '') {
+			$this->db->like('amp_family.Sequence', $sequence);
+		}
+		if ($minLength != 1) {
+			$this->db->where('amp_family.Length >=', $minLength);
+		}
+		if ($maxLength != 100) {
+			$this->db->where('amp_family.Length <=', $maxLength);
+		}
+		if ($minpI != 0) {
+			$this->db->where('amp_feature.pI >=', $minpI);
+		}
+		if ($maxpI != 15) {
+			$this->db->where('amp_feature.pI <=', $maxpI);
+		}
+		if ($minCharge != -50) {
+			$this->db->where('amp_feature.Charge >=', $minCharge);
+		}
+		if ($maxCharge != 50) {
+			$this->db->where('amp_feature.Charge <=', $maxCharge);
+		}
+		return $this->db->count_all_results();
+	}
+}
