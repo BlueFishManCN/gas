50c5ec61430a5c18c1c39d85f2a0ecf32204c390
jerry's mac <jerrychangcn@outlook.com>
2020-06-02 22:41:38 +0800

    improved UX.

--- application/controllers/Environment.php
+++ application/controllers/Environment.php
@@ -10,6 +10,7 @@ class Environment extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->model('Environment_amp_model');
+		$this->load->model('Amp_environment_model');
 		$this->load->model('Family_environment_model');
 	}
 
@@ -20,17 +21,95 @@ class Environment extends CI_Controller
 	{
 		if ($this->input->method(TRUE) == 'GET') {
 			$environment = $this->input->get('environment', TRUE);
-			$pageSize = $this->input->get('pageSize', TRUE);
-			$currentPage = $this->input->get('currentPage', TRUE);
+			$sPageSize = $this->input->get('sPageSize', TRUE);
+			$sCurrentPage = $this->input->get('sCurrentPage', TRUE);
+			$fPageSize = $this->input->get('fPageSize', TRUE);
+			$fCurrentPage = $this->input->get('fCurrentPage', TRUE);
 
 			if ($environment != '') {
 				$data['Environment_AMP'] = $this->Environment_amp_model->index($environment);
-				$data['Count'] = $this->Family_environment_model->count($environment);
-				$data['Environment_Family'] = $this->Family_environment_model->getByEnvironment($environment, $pageSize, $currentPage);
+				$data['sCount'] = $this->Amp_environment_model->count($environment);
+				$data['Environment_Sequence'] = $this->Amp_environment_model->getByEnvironment($environment, $sPageSize, $sCurrentPage);
+				$data['fCount'] = $this->Family_environment_model->count($environment);
+				$data['Environment_Family'] = $this->Family_environment_model->getByEnvironment($environment, $fPageSize, $fCurrentPage);
 
 				if ($data['Environment_AMP'] != null) {
 					$data['Environment_AMP'][0]["Length_Avg"] = trim(rtrim(sprintf("%.4f", $data['Environment_AMP'][0]["Length_Avg"]), '0'), '.');
 				}
+
+				if ($data['Environment_Family'] != null) {
+					foreach ($data['Environment_Family'] as $key => $value) {
+						$data['Environment_Family'][$key]["Length_Avg"] = trim(rtrim(sprintf("%.4f", $value["Length_Avg"]), '0'), '.');
+					}
+				}
+
+				$this->output
+					->set_status_header(200)
+					->set_content_type('application/json')
+					->set_output(json_encode($data));
+			} else {
+				$this->output
+					->set_status_header(204)
+					->set_content_type('application/json');
+			}
+		} elseif ($this->input->method(TRUE) == 'OPTIONS') {
+			$this->output
+				->set_status_header(200)
+				->set_content_type('application/json');
+		} else {
+			$this->output
+				->set_status_header(405)
+				->set_content_type('application/json');
+		}
+	}
+
+	/**
+	 *
+	 */
+	public function sequence()
+	{
+		if ($this->input->method(TRUE) == 'GET') {
+			$environment = $this->input->get('environment', TRUE);
+			$sPageSize = $this->input->get('sPageSize', TRUE);
+			$sCurrentPage = $this->input->get('sCurrentPage', TRUE);
+
+			if ($environment != '') {
+				$data['sCount'] = $this->Amp_environment_model->count($environment);
+				$data['Environment_Sequence'] = $this->Amp_environment_model->getByEnvironment($environment, $sPageSize, $sCurrentPage);
+
+				$this->output
+					->set_status_header(200)
+					->set_content_type('application/json')
+					->set_output(json_encode($data));
+			} else {
+				$this->output
+					->set_status_header(204)
+					->set_content_type('application/json');
+			}
+		} elseif ($this->input->method(TRUE) == 'OPTIONS') {
+			$this->output
+				->set_status_header(200)
+				->set_content_type('application/json');
+		} else {
+			$this->output
+				->set_status_header(405)
+				->set_content_type('application/json');
+		}
+	}
+
+	/**
+	 *
+	 */
+	public function family()
+	{
+		if ($this->input->method(TRUE) == 'GET') {
+			$environment = $this->input->get('environment', TRUE);
+			$fPageSize = $this->input->get('fPageSize', TRUE);
+			$fCurrentPage = $this->input->get('fCurrentPage', TRUE);
+
+			if ($environment != '') {
+				$data['fCount'] = $this->Family_environment_model->count($environment);
+				$data['Environment_Family'] = $this->Family_environment_model->getByEnvironment($environment, $fPageSize, $fCurrentPage);
 
 				if ($data['Environment_Family'] != null) {
 					foreach ($data['Environment_Family'] as $key => $value) {
--- application/models/Amp_environment_model.php
+++ application/models/Amp_environment_model.php
@@ -23,4 +23,32 @@ class Amp_environment_model extends CI_Model
 		$this->db->where('AMP_ID', $ampId);
 		return $this->db->get()->result_array();
 	}
+
+	/**
+	 * @param $environment
+	 * @return mixed
+	 */
+	public function count($environment)
+	{
+		$this->db->select('*');
+		$this->db->from('amp_environment');
+		$this->db->where($environment . ' >', 0);
+		return $this->db->count_all_results();
+	}
+
+	/**
+	 * @param $environment
+	 * @param $pageSize
+	 * @param $currentPage
+	 * @return mixed
+	 */
+	public function getByEnvironment($environment, $pageSize, $currentPage)
+	{
+		$this->db->select('*');
+		$this->db->from('amp_environment');
+		$this->db->join('amp_family', 'amp_family.AMP_ID=amp_environment.AMP_ID', 'left');
+		$this->db->where($environment . ' >', 0);
+		$this->db->limit($pageSize, ($currentPage - 1) * $pageSize);
+		return $this->db->get()->result_array();
+	}
 }
